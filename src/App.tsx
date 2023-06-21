7f292b4268ad6631b7fda9865d9a5e66a0c64ebf
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-06-21 10:52:09 +0900

    react-routerを導入した。

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
 import "./App.module.scss";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <h1 className="h1">動作しています！</h1>
-    </>
+      <Route path="/">
+        <p>トップページがきます。</p>
+      </Route>
+      <Route path="/learn">
+        <p>学習ページがきます。</p>
+      </Route>
+    </BrowserRouter>
   );
 }
 
