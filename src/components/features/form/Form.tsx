0617b8b1d2a38f2a8a400025c3981059437c09e3
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-06-23 14:57:36 +0900

    フォームデータのpartTypeによって、表示させるフォームの種類を変えられるようにした。

--- src/components/features/form/Form.tsx
+++ src/components/features/form/Form.tsx
@@ -1,3 +1,40 @@
+import { FormProvider } from "./FormProvider";
+import { FormData } from "../../types/formData";
+
 export const Form = () => {
-  return <h1>Form画面です。</h1>;
+  //開発で使用するサンプルのフォームデータ
+  const sampleFormData: FormData[] = [
+    {
+      id: 1,
+      partType: "0",
+      explanation: "パートの解説",
+      childrenPart: "none",
+      inputData: "入力される内容",
+    },
+    {
+      id: 2,
+      partType: "0",
+      explanation: "パートの解説",
+      childrenPart: "none",
+      inputData: "入力される内容",
+    },
+  ];
+
+  return (
+    <>
+      {sampleFormData.map((data) => {
+        return (
+          <>
+            <FormProvider
+              key={data.id}
+              partType={data.partType}
+              childrenPart={data.childrenPart}
+              inputData={data.inputData}
+            />
+            <br />
+          </>
+        );
+      })}
+    </>
+  );
 };
---
+++ src/components/features/form/formComponents/Process.tsx
@@ -0,0 +1,3 @@
+export const Process = () => {
+  return <textarea cols={40} rows={4}></textarea>;
+};
