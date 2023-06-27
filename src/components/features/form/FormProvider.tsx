e50f23fe5426557f4878c9204b773315c444f1ce
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-06-27 12:56:31 +0900

    FormProviderにForを追加した。

--- src/components/features/form/FormProvider.tsx
+++ src/components/features/form/FormProvider.tsx
@@ -1,4 +1,6 @@
 import { FormData } from "../../types/formData";
+import { For } from "./formComponents/For";
+
 import { Process } from "./formComponents/Process";
 
 type Props = {
@@ -12,5 +14,7 @@ export const FormProvider = (props: Props) => {
   //受け取ったpartTypeによって、表示するフォームを変更する
   if (formData.partType == "PROC") {
     return <Process partType={formData.partType} />;
+  } else if (formData.partType == "FOR") {
+    return <For />;
   }
 };
