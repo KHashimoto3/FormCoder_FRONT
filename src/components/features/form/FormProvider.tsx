ec206d0f3237d91689a1de57947636b53d4e63d7
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-06-30 12:14:05 +0900

    main関数コンポーネントを作成し、サンプルのフォームデータも変更した。

--- src/components/features/form/FormProvider.tsx
+++ src/components/features/form/FormProvider.tsx
@@ -4,6 +4,7 @@ import { Process } from "./formComponents/Process";
 import { For } from "./formComponents/For";
 import { While } from "./formComponents/While";
 import { Function } from "./formComponents/Function";
+import { Main } from "./formComponents/Main";
 
 type Props = {
   partType: string;
@@ -14,7 +15,11 @@ type Props = {
 export const FormProvider = (props: Props) => {
   const formData = props;
   //受け取ったpartTypeによって、表示するフォームを変更する
-  if (formData.partType == "PROC") {
+  if (formData.partType == "MAIN") {
+    return (
+      <Main partType={formData.partType} childrenPart={formData.childrenPart} />
+    );
+  } else if (formData.partType == "PROC") {
     return <Process partType={formData.partType} />;
   } else if (formData.partType == "FOR") {
     return (
