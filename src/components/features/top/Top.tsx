b2efd7e9e27bdfc118e17e111a4ecbd1d58ca1bb
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-07-06 21:26:14 +0900

    ロップページを左右２段組にした。

--- src/components/features/top/Top.tsx
+++ src/components/features/top/Top.tsx
@@ -1,3 +1,16 @@
+import { Box, Grid } from "@mui/material";
+
 export const Top = () => {
-  return <h1>トップページです。</h1>;
+  return (
+    <Box sx={{ height: "500px" }}>
+      <Grid container spacing={2}>
+        <Grid item xs={6} sx={{ background: "#c4dbff" }}>
+          <h1>左側</h1>
+        </Grid>
+        <Grid item xs={6} sx={{ background: "#ffc4fa" }}>
+          <h1>右側</h1>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
