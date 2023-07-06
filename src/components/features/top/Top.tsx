6f7303b8db37924d3b987b9c7c36e6e573424afc
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-07-07 08:09:02 +0900

    トップページにキャッチフレーズを入れた。

--- src/components/features/top/Top.tsx
+++ src/components/features/top/Top.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 
 export const Top = () => {
   const backgroundStyle = {
@@ -11,7 +11,9 @@ export const Top = () => {
       <Grid container spacing={2}>
         <Grid item xs={6}>
           <Box sx={{ textAlign: "center", paddingTop: "200px" }}>
-            <h1>左側</h1>
+            <Typography variant="h3" sx={{ fontWeight: "bold" }}>
+              フォームを使ったプログラミング学習
+            </Typography>
           </Box>
         </Grid>
         <Grid item xs={6}>
