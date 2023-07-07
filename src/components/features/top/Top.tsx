48a021fd378f323420b6f8942779fbea7bb49736
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-07-07 09:54:15 +0900

    トップページの背景を変更した。

--- src/components/features/top/Top.tsx
+++ src/components/features/top/Top.tsx
@@ -3,11 +3,11 @@ import { Box, Grid, Typography } from "@mui/material";
 export const Top = () => {
   const backgroundStyle = {
     background:
-      "linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(201,227,249,1) 100%)",
+      "linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(236,246,255,1) 100%)",
   };
 
   return (
-    <Box sx={{ height: "500px" }} style={backgroundStyle}>
+    <Box sx={{ height: "1000px" }} style={backgroundStyle}>
       <Grid container spacing={2}>
         <Grid item xs={6}>
           <Box sx={{ textAlign: "center", paddingTop: "200px" }}>
