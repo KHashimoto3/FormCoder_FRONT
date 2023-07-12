c9e8c1f16c8c32abe9f6ebcc8e2c2a7c327179c6
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-07-12 18:51:25 +0900

    Blobを使って、jsonファイルを生成する様にした。

--- src/components/pages/StorageTestPage.tsx
+++ src/components/pages/StorageTestPage.tsx
@@ -4,12 +4,22 @@ import { getStorage, ref } from "firebase/storage";
 const storage = getStorage();
 
 // Create a storage reference from our storage service
-const storageRef = ref(storage, "test");
+const storageRef = ref(storage, "test/test.json");
 
 export const StorageTestPage = () => {
+  const uploadFileTest = (text: string) => {
+    const obj = { text: text };
+    const blob = new Blob([JSON.stringify(obj, null, 2)], {
+      type: "application/json",
+    });
+  };
+
   return (
     <>
       <h1>Cloud Storage テスト用ページです。</h1>
+      <button onClick={() => uploadFileTest("アップロード完了！")}>
+        １度だけ押してください（アップロード）
+      </button>
     </>
   );
 };
