b1aa450694e03035ce57fccfd44c7e8021735c53
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-07-14 15:08:26 +0900

    フォームデータを取ってこれる様にした。

--- src/components/features/form/Form.tsx
+++ src/components/features/form/Form.tsx
@@ -4,35 +4,11 @@ import { inputData } from "../../types/inputData";
 import { useContext, useEffect } from "react";
 import { InputContext } from "./InputArrayProvider";
 
+import { storage } from "../../../firebase";
+import { getDownloadURL, ref } from "firebase/storage";
+
 export const Form = () => {
   const { initInputArray } = useContext(InputContext);
-
-  const sampleInputData: inputData[] = [
-    {
-      partType: "PROC",
-      inputDataArray: [""],
-    },
-    {
-      partType: "PROC",
-      inputDataArray: [""],
-    },
-    {
-      partType: "IF",
-      inputDataArray: [""],
-    },
-    {
-      partType: "PROC",
-      inputDataArray: [""],
-    },
-    {
-      partType: "IFE",
-      inputDataArray: [""],
-    },
-  ];
-
-  useEffect(() => {
-    initInputArray(sampleInputData);
-  }, []);
 
   //開発で使用するサンプルのフォームデータ
   const sampleFormData: FormData[] = [
@@ -81,6 +57,71 @@ export const Form = () => {
       inputIdx: -1,
     },
   ];
+
+  const sampleInputData: inputData[] = [
+    {
+      partType: "PROC",
+      inputDataArray: [""],
+    },
+    {
+      partType: "PROC",
+      inputDataArray: [""],
+    },
+    {
+      partType: "IF",
+      inputDataArray: [""],
+    },
+    {
+      partType: "PROC",
+      inputDataArray: [""],
+    },
+    {
+      partType: "IFE",
+      inputDataArray: [""],
+    },
+  ];
+
+  useEffect(() => {
+    initInputArray(sampleInputData);
+    getFormData("sampleForm");
+  }, []);
+
+  const getFormData = (formName: string) => {
+    const refUrl = "form/" + formName + ".json";
+    getFileUrl(refUrl);
+  };
+
+  const getFileUrl = (refUrl: string) => {
+    getDownloadURL(ref(storage, refUrl))
+      .then((url) => {
+        getJsonFile(url);
+      })
+      .catch((error) => {
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+          case "storage/object-not-found":
+            alert("ファイルが見つかりません！");
+            break;
+          case "storage/unauthorized":
+            alert("このファイルへのアクセス権限がありません！");
+            break;
+          case "storage/canceled":
+            alert("ユーザーはアップロードをキャンセルしました。");
+            break;
+          case "storage/unknown":
+            alert("不明なエラーが発生しました！");
+            break;
+        }
+      });
+  };
+
+  const getJsonFile = (url: string) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) => console.log(json.formData))
+      .catch(() => alert("エラーが発生しました。"));
+  };
 
   return (
     <>
