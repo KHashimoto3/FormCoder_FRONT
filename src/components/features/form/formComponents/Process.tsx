9f8648262d84c245ba398e61ea7a200f6a4f214a
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-07-18 13:08:40 +0900

    Processのフォーカスしている間、タイマーが動く様にした。

--- src/components/features/form/formComponents/Process.tsx
+++ src/components/features/form/formComponents/Process.tsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
 import { HintContext } from "../../hint/HintProvider";
 import { InputContext } from "../InputArrayProvider";
+import useInterval from "../hooks/useinterval";
 
 type Props = {
   partType: string;
@@ -15,6 +16,20 @@ export const Process = (props: Props) => {
   const { upDateInputArray } = useContext(InputContext);
 
   const [input, setInput] = useState<string>("");
+
+  //interval
+  const [count, setCount] = useState<number>(0);
+  const [delay, setDelay] = useState<number>(1000);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+
+  //intervalを使うためのカスタムフック
+  useInterval(
+    () => {
+      console.log("render:", count);
+      setCount(count + 1);
+    },
+    isRunning ? delay : null
+  );
 
   //upDateInputArrayにstringの配列を渡す
   const updateInput = (idx: number, input: string) => {
@@ -31,6 +46,7 @@ export const Process = (props: Props) => {
       onFocus={() => {
         setCurrentPartType(partType);
         setHintTypeC(explanation);
+        setIsRunning(true);
       }}
       value={input}
       onChange={(
@@ -38,7 +54,10 @@ export const Process = (props: Props) => {
       ) => {
         setInput(event.target.value);
       }}
-      onBlur={() => updateInput(props.inputIdx, input)}
+      onBlur={() => {
+        updateInput(props.inputIdx, input);
+        setIsRunning(false);
+      }}
     ></textarea>
   );
 };
