a61ffde30ee5fa79a0937365216a7b137c95dabc
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-07-20 17:30:47 +0900

    ライプCのヒントの表示を整えた。

--- src/components/features/hint/hintComponents/TypeC.tsx
+++ src/components/features/hint/hintComponents/TypeC.tsx
@@ -8,7 +8,6 @@ export const TypeC = (props: Props) => {
   const explanation = props.explanation;
   return (
     <>
-      <Typography variant="h6">解説</Typography>
       <Typography variant="body1">{explanation}</Typography>
     </>
   );
