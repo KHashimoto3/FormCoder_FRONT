3f2ad46c76fc5fde5acf4e25972b7b8265633f77
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-10-02 16:19:55 +0900

    とりあえずの項目でチェックボックスを配置してみた。

--- src/components/features/reason-check/ReasonCheck.tsx
+++ src/components/features/reason-check/ReasonCheck.tsx
@@ -1,3 +1,5 @@
+import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
+
 export const ReasonCheck = () => {
   return (
     <>
@@ -6,6 +8,16 @@ export const ReasonCheck = () => {
       <div>
         <h2>エラーが発生する場合</h2>
         <p>エラーが出てコンパイルができないまたは実行時に例外が発生する。</p>
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox />}
+            label="いずれかの行で、;（セミコロン）を書き忘れている"
+          />
+          <FormControlLabel
+            control={<Checkbox />}
+            label="ifやforといった制御の条件の直後に、不要な ;（セミコロン）を書いている"
+          />
+        </FormGroup>
       </div>
       <div>
         <h2>実行結果が違う場合</h2>
