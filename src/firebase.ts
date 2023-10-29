0965c43e67f10396aa26639f44e31ff02e32a54e
KHashimoto3 <hashi.koki385@gmail.com>
2023-10-29 19:45:44 +0900

    コメントを追加した。

--- src/firebase.ts
+++ src/firebase.ts
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: import.meta.env.FIREBASE_API_KEY,
   authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.FIREBASE_PROJECT_ID,
-  storageBucket: "formcoder-77286.appspot.com",
+  storageBucket: "formcoder-77286.appspot.com", //環境変数にするとエラーが出るため直書きで対処
   messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.FIREBASE_APP_ID,
 };
