f9f44541599aa410e11c0532b76ebb9a558efd05
KHashimoto3 <hashi.koki385@gmail.com>
2023-11-11 13:16:58 +0900

    とりあえずカードを表示させた。

--- src/components/features/leaning/Learning.tsx
+++ src/components/features/leaning/Learning.tsx
@@ -1,8 +1,23 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, Grid } from "@mui/material";
+import { FormCard } from "./FormCard";
 
 export const Learning = () => {
   return (
     <Box sx={{ height: "1000px", paddingTop: "100px" }}>
+      <Grid container spacing={1}>
+        <Grid item xs={3}>
+          <FormCard />
+        </Grid>
+        <Grid item xs={3}>
+          <FormCard />
+        </Grid>
+        <Grid item xs={3}>
+          <FormCard />
+        </Grid>
+        <Grid item xs={3}>
+          <FormCard />
+        </Grid>
+      </Grid>
       <Button
         size="large"
         onClick={() => {
