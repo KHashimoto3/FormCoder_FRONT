4b8555ca602a823b49a4f9149fc7afa2a9df39a3
KHashimoto3 <hashi.koki385@gmail.com>
2023-11-11 16:36:16 +0900

    ログイン画面を作成した。

--- src/components/features/login/Login.tsx
+++ src/components/features/login/Login.tsx
@@ -1,3 +1,55 @@
+import { Button, Container, Stack, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+
 export const Login = () => {
-  return <h1>ログイン</h1>;
+  const buttonStyle = {
+    color: "#fff",
+    background:
+      "linear-gradient(90deg, rgba(51,202,255,1) 0%, rgba(0,118,249,1) 100%)",
+    boxShadow: "0 3px 5px 0 rgba(0, 0, 0, .3)",
+  };
+
+  const [userId, setUserId] = useState<string>("");
+  const [userPassword, setUserPassword] = useState<string>("");
+
+  return (
+    <div>
+      <Container maxWidth="sm">
+        <Typography variant="h4" component="div" gutterBottom>
+          ログイン
+        </Typography>
+        <Stack spacing={2}>
+          <TextField
+            id="user-id"
+            label="ユーザID"
+            variant="standard"
+            required
+            fullWidth
+            onChange={(e) => setUserId(e.target.value)}
+          />
+          <TextField
+            id="user-password"
+            label="パスワード"
+            type="password"
+            variant="standard"
+            required
+            fullWidth
+            onChange={(e) => setUserPassword(e.target.value)}
+          />
+          <Button variant="contained" sx={buttonStyle} fullWidth>
+            {" "}
+            ログイン{" "}
+          </Button>
+        </Stack>
+        <Stack spacing={2} sx={{ marginTop: "20px" }}>
+          <Typography variant="body2" color="text.secondary" align="center">
+            アカウントをお持ちでない方
+          </Typography>
+          <Button variant="outlined" fullWidth>
+            新規登録
+          </Button>
+        </Stack>
+      </Container>
+    </div>
+  );
 };
