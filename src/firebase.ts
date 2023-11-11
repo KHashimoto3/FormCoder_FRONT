6708a91061382e3c387ca67b1a83ac5c581e93fc
KHashimoto3 <hashi.koki385@gmail.com>
2023-11-11 16:40:41 +0900

    firebase/authを宣言した。

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,5 +19,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // Initialize Cloud Storage and get a reference to the service
 const storage = getStorage(app);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
 
 export { storage };
