0b26d5dfee32b67b66298904780cd5be2081bfd1
KHashimoto3 <hashi.koki385@gmail.com>
2024-01-19 13:29:35 +0900

    フォームのリストを取得できるようにした。

--- src/components/features/leaning/Learning.tsx
+++ src/components/features/leaning/Learning.tsx
@@ -1,9 +1,18 @@
 import { Box, Grid, Typography } from "@mui/material";
 import { FormCard } from "./FormCard";
 import { FormCardList } from "../../types/formCardList";
+import { useEffect, useState } from "react";
 
 export const Learning = () => {
-  const formCardList: FormCardList[] = [
+  const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+  console.log(apiBaseUrl);
+  const [formList, setFormList] = useState<FormCardList[]>([]);
+
+  useEffect(() => {
+    pullFormList();
+  }, []);
+
+  const sampleFormList: FormCardList[] = [
     {
       id: 1,
       title: "Experiment1",
@@ -18,6 +27,40 @@ export const Learning = () => {
     },
   ];
 
+  const pullFormList = async () => {
+    const url = `${apiBaseUrl}/form/list`;
+    try {
+      await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then(async (res) => {
+        if (!res.ok) {
+          const statusCode = res.status;
+          switch (statusCode) {
+            case 400:
+              throw new Error("Bad Request");
+            case 401:
+              throw new Error("Unauthorized");
+            case 404:
+              throw new Error("Not Found");
+            case 500:
+              throw new Error("Internal Server Error");
+            default:
+              throw new Error("Unknown Error");
+          }
+        }
+        const data = await res.json();
+        setFormList(data.formList);
+      });
+    } catch (error) {
+      alert("エラーが発生しました。" + error);
+      alert("代わりにサンプルのリストを表示します。");
+      setFormList(sampleFormList);
+    }
+  };
+
   return (
     <Box sx={{ height: "1000px", paddingTop: "160px" }}>
       <Typography
@@ -29,12 +72,12 @@ export const Learning = () => {
         すべてのフォーム
       </Typography>
       <Grid container spacing={1}>
-        {formCardList.map((formCard) => (
-          <Grid item xs={3} key={formCard.id}>
+        {formList.map((form) => (
+          <Grid item xs={3} key={form.id}>
             <FormCard
-              title={formCard.title}
-              description={formCard.description}
-              url={formCard.url}
+              title={form.title}
+              description={form.description}
+              url={form.url}
             />
           </Grid>
         ))}
