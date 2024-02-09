d784cc0f05bb5e18987e6d20dd0f86a270b2a240
KHashimoto3 <hashi.koki385@gmail.com>
2024-02-09 20:53:41 +0900

    問題表示のページには、サンプルの問題を表示するようにした。

--- src/components/features/question/Question.tsx
+++ src/components/features/question/Question.tsx
@@ -1,7 +1,42 @@
+import { Typography } from "@mui/material";
+
 export const Question = () => {
+  const questionTitle = "Even or Odd（サンプル）";
+  const questionInstruction =
+    "与えられた数値が、偶数か奇数かを判定する関数を作成してください。偶数の場合はEven、奇数の場合はOddを返してください。";
+  const inputExample = "2";
+  const outputExample = "Even";
   return (
     <div>
-      <h1>Question</h1>
+      <Typography variant="h4" fontWeight={"bold"} gutterBottom>
+        {questionTitle}
+      </Typography>
+      <div style={{ background: "#ededed" }}>
+        <Typography variant="h5" gutterBottom>
+          問題:
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          {questionInstruction}
+        </Typography>
+      </div>
+      <Typography variant="h5" gutterBottom>
+        入力例:
+      </Typography>
+      <textarea
+        rows={4}
+        cols={20}
+        value={inputExample}
+        style={{ fontSize: "16pt" }}
+      />
+      <Typography variant="h5" gutterBottom>
+        出力例:
+      </Typography>
+      <textarea
+        rows={4}
+        cols={20}
+        value={outputExample}
+        style={{ fontSize: "16pt" }}
+      />
     </div>
   );
 };
