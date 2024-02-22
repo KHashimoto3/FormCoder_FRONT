0f1b5a8a25f284255000491ec86c93a9a40ae400
KHashimoto3 <hashi.koki385@gmail.com>
2024-02-22 10:38:34 +0900

    Dashboard用のタイトルバーを作成して、配置した。

--- src/components/features/dashboard/Dashboard.tsx
+++ src/components/features/dashboard/Dashboard.tsx
@@ -2,13 +2,14 @@ import { Grid } from "@mui/material";
 import { DashboardMenu } from "./DashboardMenu";
 import { DashboardContent } from "./DashboardContent";
 import { useState } from "react";
+import { DashboardTitleBar } from "./DashboardTitleBar";
 
 export const Dashboard = () => {
   const [selectedMenu, setSelectedMenu] = useState<string>("学習");
 
   return (
     <div>
-      <h1>Dashboard</h1>
+      <DashboardTitleBar />
       <Grid container spacing={2}>
         <Grid item xs={2}>
           <DashboardMenu
---
+++ src/components/features/dashboard/DashboardTitleBar.tsx
@@ -0,0 +1,45 @@
+import {
+  Avatar,
+  Badge,
+  Button,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+export const DashboardTitleBar = () => {
+  return (
+    <div
+      style={{
+        background: "#ffffff",
+        height: "10%",
+        borderBottom: "0.5px solid #000000",
+      }}
+    >
+      <Grid container spacing={1}>
+        <Grid item xs={10}>
+          <Typography variant="h4">FormCoder Dashboard</Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <IconButton
+            size="large"
+            aria-label="show 4 new mails"
+            color="inherit"
+          >
+            <Badge badgeContent={4} color="error">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+          <IconButton size="small" aria-label="your name" color="inherit">
+            <Button size="small">
+              <Avatar sx={{ width: 30, height: 30 }}>N</Avatar>
+              <Typography variant="body1" sx={{ color: "#000000" }}>
+                ユーザー名
+              </Typography>
+            </Button>
+          </IconButton>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
