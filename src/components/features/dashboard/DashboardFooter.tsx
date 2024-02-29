8b8f2ecdb45bfe2723992bde20d020f49b9c7cfa
KHashimoto3 <hashi.koki385@gmail.com>
2024-02-29 15:18:12 +0900

    ダッシュボードヘッダーのリンクの示し方をシンプルにした。

--- src/components/features/dashboard/DashboardFooter.tsx
+++ src/components/features/dashboard/DashboardFooter.tsx
@@ -4,7 +4,7 @@ export const DashboardFooter = () => {
   return (
     <div style={{ background: "#ffffff", height: "auto" }}>
       <Typography variant="body1" sx={{ color: "#000000" }}>
-        Form Coder <a href="/">システムについて</a>
+        <a href="/">Form Coder</a>
       </Typography>
     </div>
   );
