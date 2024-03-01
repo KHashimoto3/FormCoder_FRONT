9f6cce0b6c09852a426aa440d05fc899b5b07a4c
KHashimoto3 <hashi.koki385@gmail.com>
2024-03-01 16:38:11 +0900

    保存されたフォームの一覧をサンプルで表示するようにした。

--- src/components/features/dashboard/DashboardContent.tsx
+++ src/components/features/dashboard/DashboardContent.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const DashboardContent = (props: Props) => {
   const { selectedMenu } = props;
   return (
-    <div style={{ background: "#f2f2f2", height: "700px" }}>
+    <div style={{ background: "#f2f2f2", height: "700px", width: "100%" }}>
       <DashboardContentProvider selectedMenu={selectedMenu} />
     </div>
   );
--- src/components/features/dashboard/dashboardContents/DBEdit.tsx
+++ src/components/features/dashboard/dashboardContents/DBEdit.tsx
@@ -1,9 +1,57 @@
-import { Typography } from "@mui/material";
+import {
+  Button,
+  Grid,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DescriptionIcon from "@mui/icons-material/Description";
 
 export const DBEdit = () => {
+  const listStyle = {
+    width: "100%",
+    height: "auto",
+    background: "#ffffff",
+  };
+
   return (
     <div>
-      <Typography variant="h3">編集</Typography>
+      <Typography variant="h4">編集</Typography>
+      <Grid item xs={12}>
+        <div style={listStyle}>
+          <List>
+            <ListItem>
+              <ListItemIcon>
+                <DescriptionIcon />
+              </ListItemIcon>
+              <ListItemText primary="サンプルのフォーム1" />
+              <Button
+                variant="contained"
+                color="primary"
+                startIcon={<EditIcon />}
+              >
+                編集
+              </Button>
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <DescriptionIcon />
+              </ListItemIcon>
+              <ListItemText primary="サンプルのフォーム2" />
+              <Button
+                variant="contained"
+                color="primary"
+                startIcon={<EditIcon />}
+              >
+                編集
+              </Button>
+            </ListItem>
+          </List>
+        </div>
+      </Grid>
     </div>
   );
 };
